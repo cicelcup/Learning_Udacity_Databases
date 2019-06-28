d4a2a0efc1d7924d9dc686a3f910e81fd3dce171
JAPM <cicelcup@gmail.com>
2019-06-28 15:10:50 -0600

    Creating the query method

--- app/src/main/java/com/example/android/pets/data/PetsProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetsProvider.java
@@ -1,15 +1,18 @@
 package com.example.android.pets.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 //Content Provider for the database
 public class PetsProvider extends ContentProvider {
     //Database Helper
     private PetsHelper petsHelper;
+    private SQLiteDatabase db;
 
     //URI Information
     private static final int PETS = 100;
@@ -17,10 +20,11 @@ public class PetsProvider extends ContentProvider {
     private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
     static {
-        sUriMatcher.addURI(PetsContract.CONTENT_AUTHORITY,PetsContract.PATH_PETS,PETS);
-        sUriMatcher.addURI(PetsContract.CONTENT_AUTHORITY,PetsContract.PATH_PETS + "/#",
+        sUriMatcher.addURI(PetsContract.CONTENT_AUTHORITY, PetsContract.PATH_PETS, PETS);
+        sUriMatcher.addURI(PetsContract.CONTENT_AUTHORITY, PetsContract.PATH_PETS + "/#",
                 PETS_ID);
     }
+
     //Initialize the content provider
     @Override
     public boolean onCreate() {
@@ -32,7 +36,46 @@ public class PetsProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
-        return null;
+
+        //Enable the DB
+        db = petsHelper.getReadableDatabase();
+
+        //Open the cursor
+        Cursor queryCursor;
+
+        //Check the URI arrived
+        int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case PETS:
+                queryCursor = db.query(PetsContract.PetsEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            case PETS_ID:
+                //Select the id
+                selection = PetsContract.PetsEntry._ID + "=?";
+                // Select value to filter
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                //do the query
+                queryCursor = db.query(PetsContract.PetsEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+
+
+        return queryCursor;
     }
 
     //Returns the MIME type of data for the content URI
