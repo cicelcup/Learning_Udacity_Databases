dde78880b1471885d4ad843369800bc9fd93858c
JAPM <cicelcup@gmail.com>
2019-07-01 11:48:56 -0600

    Creating the update method in the content provider

--- app/src/main/java/com/example/android/pets/data/PetsProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetsProvider.java
@@ -9,8 +9,14 @@ import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.util.Log;
 
-import static com.example.android.pets.data.PetsContract.*;
-import static com.example.android.pets.data.PetsContract.PetsEntry.*;
+import static com.example.android.pets.data.PetsContract.CONTENT_AUTHORITY;
+import static com.example.android.pets.data.PetsContract.PATH_PETS;
+import static com.example.android.pets.data.PetsContract.PetsEntry.COLUMN_PET_GENDER;
+import static com.example.android.pets.data.PetsContract.PetsEntry.COLUMN_PET_NAME;
+import static com.example.android.pets.data.PetsContract.PetsEntry.COLUMN_PET_WEIGHT;
+import static com.example.android.pets.data.PetsContract.PetsEntry.TABLE_NAME;
+import static com.example.android.pets.data.PetsContract.PetsEntry._ID;
+import static com.example.android.pets.data.PetsContract.PetsEntry.isValidGender;
 
 //Content Provider for the database
 public class PetsProvider extends ContentProvider {
@@ -20,7 +26,7 @@ public class PetsProvider extends ContentProvider {
 
     //URI Information for decide the case (values are chosen arbitrary)
     private static final int PETS = 100;
-    private static final int PETS_ID = 101;
+    private static final int PET_ID = 101;
     //URI to identify when there's not match
     private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
     //Log Tag
@@ -32,7 +38,7 @@ public class PetsProvider extends ContentProvider {
     static {
         sUriMatcher.addURI(CONTENT_AUTHORITY, PATH_PETS, PETS);
         sUriMatcher.addURI(CONTENT_AUTHORITY, PATH_PETS + "/#",
-                PETS_ID);
+                PET_ID);
     }
 
     //Initialize the content provider
@@ -66,7 +72,7 @@ public class PetsProvider extends ContentProvider {
                         null,
                         sortOrder);
                 break;
-            case PETS_ID:
+            case PET_ID:
                 //Select the id
                 selection = _ID + "=?";
                 // Select value to filter
@@ -100,7 +106,7 @@ public class PetsProvider extends ContentProvider {
         //Check the URI arrived
         final int match = sUriMatcher.match(uri);
 
-        switch (match){
+        switch (match) {
             case PETS:
                 return insertPet(uri, values);
 
@@ -118,7 +124,41 @@ public class PetsProvider extends ContentProvider {
     //update using the content provider
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            //update the whole table
+            case PETS:
+                return updatePet(values, selection, selectionArgs);
+            //Update the selection
+            case PET_ID:
+                selection = _ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                return updatePet(values, selection, selectionArgs);
+            //throw an exception
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for: " + uri);
+
+        }
+    }
+
+    //Method to update the database
+    private int updatePet(ContentValues values, String selection, String[] selectionArgs) {
+
+        //Open the database
+        db = petsHelper.getWritableDatabase();
+
+        //Check that every field is set properly
+        if (sanityCheck(values) == true) {
+            //Check if the size of values is different than 0
+            if (values.size() != 0) {
+                return db.update(TABLE_NAME, values, selection, selectionArgs);
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+
     }
 
     //Method to insert data into the database
@@ -126,35 +166,42 @@ public class PetsProvider extends ContentProvider {
         //Enable to write the database
         db = petsHelper.getWritableDatabase();
 
-        //Data Sanity Check
+        //Check the data
+        if (sanityCheck(values) == true) {
+            long id = db.insert(TABLE_NAME, null, values);
+
+            if (id == -1) {
+                Log.e(LOG_TAG, "Failed to insert row for " + uri);
+                return null;
+            }
+            //Insert the value into the DB
+            return ContentUris.withAppendedId(uri, id);
+        } else {
+            return null;
+        }
+    }
+
+    //Check if the data is setting corrected it
+    private boolean sanityCheck(ContentValues values) {
+
         String name = values.getAsString(COLUMN_PET_NAME);
 
         //Check if the name is empty
-        if (name == null || name.isEmpty()){
-            return null;
+        if (name == null || name.isEmpty()) {
+            return false;
         }
 
         Integer gender = values.getAsInteger(COLUMN_PET_GENDER);
         //Check if the gender is valid or not
-        if (gender == null || !isValidGender(gender)){
-            return null;
+        if (gender == null || !isValidGender(gender)) {
+            return false;
         }
 
         Integer weight = values.getAsInteger(COLUMN_PET_WEIGHT);
         //Check if the weight is valid or not
-        if (weight == null || weight <0 ){
-            return null;
+        if (weight == null || weight < 0) {
+            return false;
         }
-
-        long id = db.insert(TABLE_NAME,null,values);
-
-        if (id == -1) {
-            Log.e(LOG_TAG, "Failed to insert row for " + uri);
-            return null;
-        }
-
-        //Insert the value into the DB
-        return ContentUris.withAppendedId(uri,id);
-
+        return true;
     }
 }
